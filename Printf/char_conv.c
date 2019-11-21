4dcd3a521007334be8ebbab8ff20c65624e16bb4
tpons@student.42.fr <tpons@student.42.fr>
2019-11-21 13:24:22 +0100

    Return value correction for char conv

--- Printf/char_conv.c
+++ Printf/char_conv.c
@@ -6,7 +6,7 @@
 /*   By: tpons <tpons@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/18 16:14:19 by tpons             #+#    #+#             */
-/*   Updated: 2019/11/19 13:52:15 by tpons            ###   ########.fr       */
+/*   Updated: 2019/11/20 17:54:43 by tpons            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,12 @@ int	char_conv(t_flags flags, va_list args)
 	{
 		ft_putchar_fd(va_arg(args, int), 1);
 		ft_adjust(flags.width - 1, ' ');
-		return (flags.width + 1);
+		return (flags.width);
 	}
 	else if (!flags.minus && flags.width > 1)
 		ft_adjust(flags.width - 1, ' ');
 	ft_putchar_fd(va_arg(args, int), 1);
-	return (flags.width + 1);
+	if (flags.width > 1)
+		return (flags.width);
+	return (1);
 }
